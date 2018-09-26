35472d264f0e11eef529970bd8681a1563f3558c
Sanchan-Misumi <kirakirasmile601@gmail.com>
2018-09-26 15:57:15 +0900

    navigationcontollor上に+ボタンが表示されない

--- todol4s/ViewController.swift
+++ todol4s/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    //todoを格納する配列
+    var todoArray = [Any]()
+
+    var saveData: UserDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        textField.text = ""
     }
 
-
+    @IBAction func save(_ sender: Any) {
+        
+        if textField.text != "" {
+        saveData.set(textField.text, forKey:"todo")
+        todoArray.append(saveData.object(forKey: "todo") as Any)
+        print(todoArray)
+        
+        performSegueToResult()
+        } else {
+            
+        }
+        
+    }
+    
+    func performSegueToResult(){
+        performSegue(withIdentifier: "toToDoViewController", sender: nil)
+    }
+    
+    //セグエを準備する時に呼ばれるメソッド
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toToDoViewController"{
+            let todoViewController = segue.destination as! todoViewController
+            todoViewController.todoArray = self.todoArray as! [String]
+        }
+    }
 }
 
---
+++ todol4s/todoViewController.swift
@@ -0,0 +1,68 @@
+//
+//  todoViewController.swift
+//  todol4s
+//
+//  Created by Maho Misumi on 2018/09/21.
+//  Copyright © 2018年 Maho Misumi. All rights reserved.
+//
+
+import UIKit
+
+class todoViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet var table: UITableView!
+    
+    var todoArray: [String] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        table.dataSource = self
+        table.delegate = self
+        print(todoArray)
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel?.text = todoArray[indexPath.row]
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(todoArray[indexPath.row])が選ばれました！")
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            todoArray.remove(at: indexPath.row)
+            table.deleteRows(at: [indexPath], with: .fade)
+        } }
+    //テーブルビュー編集時に呼ばれるメソッド
+//    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCell.EditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+//
+//        //削除の場合、配列からデータを削除する。
+//        if( editingStyle == UITableViewCell.EditingStyle.delete) {
+//            todoArray.index.removeAtIndex(indexPath.row)
+//        }
+
+//        //テーブルの再読み込み
+//        tableView.reloadData()
+//    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
